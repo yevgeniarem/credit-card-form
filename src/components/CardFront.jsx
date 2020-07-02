92cd893277bac4c37c859bfa7fae744ce2e4aea5
Yevgenia Rem <yevgeniarem@gmail.com>
2020-07-02 12:12:38 -0700

    trying to make number animations work without bugs

--- src/components/CardFront.jsx
+++ src/components/CardFront.jsx
@@ -3,40 +3,34 @@ import { useSelector } from 'react-redux';
 import { motion, AnimatePresence } from 'framer-motion';
 
 import { currentCardBackground, IMG_URL } from '../utils/constants';
-import { generateLogoUrl, determineCardMask } from '../utils/helpers';
+import { generateLogoUrl } from '../utils/helpers';
 
 const numberVariants = {
   hidden: {
     opacity: 0,
     y: 15,
+    transition: {
+      duration: 0.3,
+    },
   },
   visible: {
     opacity: 1,
     y: 0,
+    transition: {
+      duration: 0.3,
+    },
   },
   up: {
     opacity: 0,
     y: -15,
-  },
-};
-
-const maskVariants = {
-  visible: {
-    opacity: 1,
-    y: 0,
-  },
-  hidden: {
-    opacity: 0,
-    y: -15,
     transition: {
-      ease: 'easeOut',
-      duration: 0.4,
+      duration: 0.3,
     },
   },
 };
 
 export default function CardFront() {
-  const { number, name, month, year, cardType, numberAnimations } = useSelector(
+  const { number, name, month, year, cardType } = useSelector(
     (state) => state.card,
   );
 
@@ -57,44 +51,25 @@ export default function CardFront() {
         alt="credit card logo"
         className="card__logo"
       />
-      {/* <div className="card__numberMask">
-        {determineCardMask(cardType)
-          .split('')
-          .map((n, idx) => {
-            return numberAnimations.includes(idx) ? (
-              <motion.span
-                variants={maskVariants}
-                initial="visible"
-                animate="hidden"
-                // eslint-disable-next-line react/no-array-index-key
-                key={idx}
-                className="card__number"
-              >
-                {n}
-              </motion.span>
-            ) : (
-              // eslint-disable-next-line react/no-array-index-key
-              <span key={idx} className="card__number">
-                {' '}
-              </span>
-            );
-          })}
-      </div> */}
       <div className="card__numbers">
         <AnimatePresence>
           {number.split('').map((n, idx) => {
             return (
-              <motion.span
-                variants={numberVariants}
-                initial="hidden"
-                animate="visible"
-                exit="up"
+              <span
+                className="card__number-container"
                 // eslint-disable-next-line react/no-array-index-key
                 key={`${n}${idx}`}
-                className="card__number"
               >
-                {n}
-              </motion.span>
+                <motion.span
+                  variants={numberVariants}
+                  initial="hidden"
+                  animate="visible"
+                  exit="up"
+                  className="card__number"
+                >
+                  {n}
+                </motion.span>
+              </span>
             );
           })}
         </AnimatePresence>
